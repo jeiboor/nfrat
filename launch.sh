f89f4546dbb1e31c800e95be34a12c457286de69
MR_saf <jeiboor@users.noreply.github.com>
2016-07-07 09:46:32 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -77,7 +77,7 @@ install_rocks() {
 install() {
   git pull
   git submodule update --init --recursive
-  # patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
+  patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
   RET=$?;
 
   cd tg
@@ -110,6 +110,9 @@ else
     echo "Run $0 install"
     exit 1
   fi
-
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
+  while true; do
+   rm -r ../.telegram-cli/state
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/nfratbot.lua -l 1 -E $@
+   sleep 3
+  done
 fi
